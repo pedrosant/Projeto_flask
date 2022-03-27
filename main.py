27ae9a98a0428cef9c08ff8dd40f3ee37890a98a
Pedro Roberto <roberto19santana@gmail.com>
2022-03-26 23:57:40 -0300

    notas telas html

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from pprint import pp
+import string
+from urllib.request import Request
 from flask import Flask, request, render_template, redirect, url_for
-
+from rotas import verif_rota
 app = Flask(__name__)
 
 
-@app.route('/logado')
-def logado():    
-    return render_template('home.html')
+@app.route('/logado', methods = ['POST','GET'])
+def logado():
+    rota = verif_rota('logado') 
+    print(rota)              
+    return render_template(rota)
+    
+@app.route('/usuario')
+def Usuario():
+    return render_template('usuario.html')
+
+@app.route('/passageiro')
+def Passageiro():
+    return render_template('passageiro.html')
+
+@app.route('/motorista')
+def Motorista():
+    return render_template('motorista.html')
+
+@app.route('/veiculos')
+def Veiculos():
+    return render_template('veiculos.html')
 
 
 @app.route("/", methods = ['POST','GET'])
---
+++ rotas.py
@@ -0,0 +1,11 @@
+def verif_rota(rota):
+    if rota == 'logado':
+        return 'home.html'
+    if rota == 'usuario':
+        return 'usuario.html'
+    if rota == 'passageiro':
+        return 'passageiro.html'
+    if rota == 'motorista':
+        return 'motorista.html'
+    if rota == 'veiculos':
+        return 'veiculos.html'    
